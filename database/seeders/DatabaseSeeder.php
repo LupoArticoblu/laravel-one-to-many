d160577bcf9de32bc9f66dac3e25d51c39433e4f
LupoArticoblu <wolf94-@libero.it>
2023-01-27 19:10:36 +0100

    creazione tabella ponte

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         PortfolioSeeder::class,
         CategorySeeder::class,
         CategoryPortfolioSeeder::class,
+        TagsTableSeeder::class,
         ]);
     }
 }
